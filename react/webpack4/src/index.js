e89e061da1097ca87fb75457669a02b11e46d8b3
Francisco Kahil <francisco.kahil@luizalabs.com>
2018-07-04 23:16:23 -0300

    Add initial webpack

---
+++ react/webpack4/src/index.js
@@ -0,0 +1,22 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import App from './App'
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+import _ from 'lodash';
+import './style.css';
+
+  function component() {
+    var element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hellossss', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    return element;
+  }
+
+  document.body.appendChild(component());
