13277fb79fc528986e6ca12eded9ce58c9618625
Francisco Kahil <francisco.kahil@luizalabs.com>
2018-07-07 16:40:21 -0300

    Add ci

--- react/parcel/jest.config.js
+++ react/parcel/jest.config.js
@@ -3,3 +3,10 @@ var enzyme = require('enzyme');
 var Adapter = require('enzyme-adapter-react-16');
 
 enzyme.configure({ adapter: new Adapter() });
+
+module.exports = {
+  "testPathIgnorePatterns": [
+    "<rootDir>/cypress/",
+    "<rootDir>/node_modules/"
+  ]
+};
